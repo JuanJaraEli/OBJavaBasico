5517f9f59286933257f1235613eb0fbce37bccf3
JuanJaraEli <juanmajaraelizeche97@gmail.com>
2023-05-05 22:31:59 -0400

    Ejercicios tema 1 culminado.

---
+++ Ejercicios tema 1/src/com/company/Datos.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Datos {
+
+    public static void main(String[] args) {
+
+        int entero = 1;
+        long largo = 2;
+        double doble = 2.2;
+        boolean booleano = true;
+        char caracter = 'a';
+        String cadena = "Hola";
+
+        System.out.println(entero);
+        System.out.println(largo);
+        System.out.println(doble);
+        System.out.println(booleano);
+        System.out.println(caracter);
+        System.out.println(cadena);
+    }
+}
